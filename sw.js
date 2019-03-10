007a1219bdab17429b1937659078e07465fbd21f
SurajMunna <45663367+SurajMunna@users.noreply.github.com>
2019-03-10 17:58:13 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const preCacheName = "pre-cache-calc",
+    preCacheFiles = [
+        "/",
+        "/index.html",
+        "css/navbar.css",
+        "css/style.css",
+        "css/vendor.css",
+        "css/bootstrap.min.css",
+        "images/carosel/1.jpg",
+        "images/carosel/1.png",
+        "images/carosel/2.jpg",
+        "images/carosel/2.png",
+        "images/logo/logo small.png",
+        "images/logo/logo.png",
+        "js/bootstrap.min.js" ,
+        "js/jquery.min.js",
+        "js/location.js"      
+    ];
+
+
+self.addEventListener("install", event => {
+
+    console.log("installing precache files");
+
+    caches.open(preCacheName).then(function (cache) {
+
+        return cache.addAll(preCacheFiles);
+
+    });
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request).then(response => {
+
+            if (!response) {
+
+                return fetch(event.request);
+
+            }
+
+            return response;
+
+        })
+
+    )
+
+});
